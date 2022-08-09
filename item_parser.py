712b6dfd5748defb0df5c21b299dc890340883b6
NorbertasL <nlapenas@gmail.com>
2022-08-09 16:57:51 +0100

    Improved on how items are parsed and added typing to everything

--- item_parser.py
+++ item_parser.py
@@ -1,191 +1,249 @@
-import json
+import enum
 import re
-import time
-from pprint import pprint
-from tokenize import Comment
 
+from bs4 import BeautifulSoup, ResultSet, PageElement  # type: ignore
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from bs4 import BeautifulSoup
-from item_obj import Item
-import pandas as pd
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+from item_obj import Item, ItemBasicParameterOf, ItemStatsOf, ArmourClassOf, SocketColourOf
 
 
 class SingleItemParser:
-    item_property_names = (  # This is only used for secondary stats
-        "Strength",
-        "Stamina",
-        "Agility",
-        "Intellect",
-        "Spirit",
-        "Mana Per 5",
-        "Hit",
-        "Haste",
-        "Critical Strike",
-        "Expertise",
-        "Spell Power",
-        "Spell Damage",
-        "Healing Healing"
-    )
 
-    def __init__(self, service, url):
-        self.url = url
+    def __init__(self, service: Service, url: str):
+        self.url: str = url
 
-        browser = _get_chrom_driver(service)
+        browser: WebDriver = _get_chrom_driver(service)
         browser.get(url)
-        content = browser.page_source
+        content: str = browser.page_source
 
-        self.soup_content = BeautifulSoup(content, "html.parser")
-        self.tooltip = self.soup_content.find("div", attrs={'class': 'wowhead-tooltip'})
+        self.soup_content: BeautifulSoup = BeautifulSoup(content, "html.parser")
+        self.tooltip: BeautifulSoup = self.soup_content.find("div", attrs={'class': 'wowhead-tooltip'})
+
         # Closing Virtual Chrome browser after we are done
         browser.quit()
 
-    def get_item_data(self):
-        tooltip = self.tooltip
+    def get_item_data(self) -> Item:
 
-        item_object = Item(self.get_item_name())
-        item_object.set_parameter(Item.PARAMETERS.TYPE, self.get_item_type())
-        item_object.set_parameter(Item.PARAMETERS.ARMOUR_CLASS, self.get_armour_class())
-        item_object.set_parameter(Item.PARAMETERS.CORE_STATS, self.get_core_stats())
-        item_object.set_parameter(Item.PARAMETERS.SOCKETS, self.get_sockets())
+        item_object: Item = Item(self.get_item_name())
+        item_object.set_basic_parameter(ItemBasicParameterOf.TYPE, self.get_item_type())
+        item_object.set_basic_parameter(ItemBasicParameterOf.ARMOUR_CLASS, self.get_armour_class().value)
+        item_object.add_stats(self.get_core_stats())
+        item_object.set_sockets(self.get_sockets())
+        key, value = self.get_socket_bonus()
+        if key is not None and value is not None:
+            item_object.set_socket_bonus(key, value)
 
         secondary_stat_list, proc_effect_list, user_effect_list, unknown_effect_list = self.get_secondary_stats()
 
-        item_object.set_parameter(Item.PARAMETERS.SECONDARY_STATS, secondary_stat_list)
-        item_object.set_parameter(Item.PARAMETERS.PROC_EFFECT, proc_effect_list)
-        item_object.set_parameter(Item.PARAMETERS.USE_EFFECTS, user_effect_list)
-        item_object.set_parameter(Item.PARAMETERS.OTHER, unknown_effect_list)
+        item_object.add_stats(secondary_stat_list)
+
+        # TODO Deal with proc/use and unknown effect
+        # item_object.set_parameter(ItemParameterOf.PROC_EFFECT, proc_effect_list)
+        # item_object.set_parameter(ItemParameterOf.USE_EFFECTS, user_effect_list)
+        # item_object.set_parameter(ItemParameterOf.OTHER, unknown_effect_list)
 
         return item_object
-        """
-        item_data = {
-            "Type": self.get_item_type(),
-            "Armour Class": self.get_armour_class(),
-            "Name": self.get_item_name(),
-            "Item Level": self.get_item_level()}
-        item_data.update(self.get_core_stats())
-        item_data.update(self.get_sockets())
-        item_data.update(self.get_socket_bonus())
-        item_data.update(self.get_secondary_stats())
 
-        return item_data
-        """
-
-    def get_item_name(self):
+    def get_item_name(self) -> str:
         return self.tooltip.find("b").text
 
-    def get_core_stats(self):
-        spans = self.tooltip.find_all("span")
-        core_stat_list = {}
-        for s in spans:
-            sting = s.text
-            if len(sting) == 0:
+    def get_core_stats(self) -> dict[ItemStatsOf, int]:
+        spans: ResultSet = self.tooltip.find_all("span")
+        core_stat_list: dict[ItemStatsOf, int] = {}
+        for span in spans:
+            text_string: str = span.text.strip()
+            if len(text_string) == 0:
+                # Empty field, not interested
                 continue
 
             # Spans with + have main stats in them
-            if sting[0] == "+":
-                key_name = sting.split(" ")[1]
-                core_stat_list[key_name] = re.search(r'\d+', sting).group()
-                continue
+            if text_string[0] == "+":
+                string_list: list[str] = text_string.split(" ")
+
+                if len(string_list) >= 1 or len(string_list[1]) == 0:
+                    stat_name: str = string_list[1]
+                else:
+                    print("Issue getting core stat name from:", text_string)
+                    continue
+
+                for know_stat_name in ItemStatsOf:
+                    if stat_name.lower() == know_stat_name.value.lower():
+                        temp_string: str = re.search(r'\d+', text_string).group()  # type: ignore
+                        try:
+                            value: int = int(temp_string)
+                        except ValueError:
+                            print("Issue getting core stat value from:", text_string)
+                            continue
+                        core_stat_list[know_stat_name] = value
 
         return core_stat_list
 
-    def get_secondary_stats(self):
-        spans = self.tooltip.find_all("span", attrs={"class": "q2"})
-        secondary_stat_list = {}
-        proc_effect_list = {}
-        user_effect_list = {}
-        unknown_effect_list = {}
-        for s in spans:
-            s = s.text
+    def get_secondary_stats(self) -> tuple[dict[ItemStatsOf, int], str, str, str]:
+        spans: ResultSet = self.tooltip.find_all("span", attrs={"class": "q2"})
+        secondary_stat_list: dict[ItemStatsOf, int] = {}
+        proc_effect: str = ""
+        user_effect: str = ""
+        unknown_effect: str = ""
 
-            if s.startswith("Use:"):
-                user_effect_list[Item.PARAMETERS.USE_EFFECTS.name] = s
+        for span in spans:
+            text_string: str = span.text.strip()
+
+            if text_string.startswith("Use:"):
+                # Has use effect
+                user_effect = text_string
 
             else:
-                if "proc" in s.lower():
+                if "proc" in text_string.lower():
                     # Has a proc in string so a proc effect
-                    proc_effect_list[Item.PARAMETERS.PROC_EFFECT.name] = s
+                    proc_effect = text_string
                 else:
-                    for property_name in SingleItemParser.item_property_names:
-                        if re.search(property_name, s, re.IGNORECASE):
-                            secondary_stat_list[property_name] = re.search(r'\d+', s).group()
-                            continue
-                    unknown_effect_list[Item.PARAMETERS.OTHER.name] = s
 
-        return secondary_stat_list, proc_effect_list, user_effect_list, unknown_effect_list
+                    stat: dict[ItemStatsOf, int] = {}
 
-    def get_item_level(self):
-        spans = self.tooltip.find_all("span")
-        return re.search(r'\d+', spans[0].text).group()
-
-    def get_item_type(self):
-        td = self.tooltip.find_all("td")
-        return td[2].text
-
-    def get_armour_class(self):
-        span = self.tooltip.find("span", "q1")
-        if span is None:
-            return "None"
-        return span.text if span.text else "None"
-
-    def get_sockets(self):
-        sockets = self.tooltip.find_all("a", "q0")
-        socket_list = {}
-        for s in sockets:
-            s = s.text
-            words = s.split(" ")
-            if len(words) == 0:
-                continue
-            for colour in Item.SOCKET_COLOURS:
-                if colour.value.lower() == words[0].lower():
-                    if colour in socket_list:
-                        socket_list[colour.name] = socket_list[colour.name] + 1
+                    for know_stat_name in ItemStatsOf:
+                        if re.search(know_stat_name.value, text_string, re.IGNORECASE):
+                            temp_string: str = re.search(r'\d+', text_string).group()  # type: ignore
+                            try:
+                                value: int = int(temp_string)
+                            except ValueError:
+                                print("Issue getting secondary stat value from:", text_string)
+                                continue
+                            stat[know_stat_name] = value
+                    if stat == {}:  # Didn't find a know stat so has to be unknown
+                        print("Unknown effect has been found:", text_string)
+                        unknown_effect = text_string
                     else:
-                        socket_list[colour.name] = 1
-        bonus = self.get_socket_bonus()
-        if len(bonus) > 0:
-            socket_list.update(bonus)
+                        secondary_stat_list.update(stat)
+
+        return secondary_stat_list, proc_effect, user_effect, unknown_effect
+
+    def get_item_level(self) -> int:
+        spans: ResultSet = self.tooltip.find_all("span")
+        if len(spans) == 0:
+            print("Issue finding item level:", spans)
+            return 0
+
+        temp_string: str = re.search(r'\d+', spans[0].text).group()  # type: ignore
+        try:
+            value: int = int(temp_string)
+        except ValueError:
+            print("Issue getting item level value from:", spans)
+            return 0
+        return value
+
+    def get_item_type(self) -> str:
+        # TODO Make a check to know type enum in item_obj
+        tds: ResultSet = self.tooltip.find_all("td")
+        if len(tds) < 3:
+            print("Failed to find item type in:", tds)
+        return tds[2].text
+
+    def get_armour_class(self) -> ArmourClassOf:
+        span: ResultSet = self.tooltip.find("span", "q1")
+        if span is None or span.text == "":
+            return ArmourClassOf.NONE
+
+        # Check armour class
+        for armour_class in ArmourClassOf:
+            if span.text.strip().lower() == armour_class.value.lower():
+                return armour_class
+
+        return ArmourClassOf.UNKNOWN
+
+    def get_sockets(self) -> dict[SocketColourOf, int]:
+        sockets: ResultSet = self.tooltip.find_all("a", "q0")
+        socket_list: dict[SocketColourOf, int] = {}
+        for socket in sockets:
+            text_string: str = socket.text
+            split_strings: list[str] = text_string.split(" ")
+            if len(split_strings) == 0:
+                # No socket :D
+                continue
+            for socket_colour in SocketColourOf:
+                if socket_colour.value.lower() == split_strings[0].lower():
+                    if socket_colour in socket_list:
+                        socket_list[socket_colour] = socket_list[socket_colour] + 1
+                    else:
+                        socket_list[socket_colour] = 1
+
         return socket_list
 
-    def get_socket_bonus(self):
-        socket_bonus = self.tooltip.find("span", "q0")
-        socket_bonus_pair = {}
-        if socket_bonus is None:
-            return socket_bonus_pair
-        socket_bonus = socket_bonus.text
-        for property_name in SingleItemParser.item_property_names:
-            if re.search(property_name, socket_bonus, re.IGNORECASE):
-                socket_bonus_pair[Item.PARAMETERS.SOCKET_BONUS.name] = {
-                    property_name: re.search(r'\d+', socket_bonus).group()}
-                return socket_bonus_pair
+    def get_socket_bonus(self) -> tuple[ItemStatsOf | None, int | None]:
+        socket_bonus_element: PageElement = self.tooltip.find("span", "q0")
+
+        if socket_bonus_element is None:
+            return None, None
+
+        socket_bonus = socket_bonus_element.text.lower()
+
+        stat_name: ItemStatsOf | None = None
+        value: int | None = None
+        for stat in ItemStatsOf:
+            if re.search(stat.value, socket_bonus, re.IGNORECASE):
+                temp_string: str = re.search(r'\d+', socket_bonus).group()  # type: ignore
+                try:
+                    value = int(temp_string)
+                except ValueError:
+                    print("Issue getting socket bonus stat value from:", socket_bonus)
+                    continue
+                stat_name = stat
+                value = value
+
+        return stat_name, value
 
 
 class ItemListParser:
     def __init__(self, service, url):
-        self.url = url
-        browser = _get_chrom_driver(service)
+        self.url: str = url
+        browser: WebDriver = _get_chrom_driver(service)
         browser.get(url)
 
-        # TODO make this dynamic, use id = main-contents
-        # time.sleep(5)
-        content = browser.page_source
+        content: str = browser.page_source
 
-        self.soup_content = BeautifulSoup(content, "html.parser")
-        # pprint(self.soup_content)
+        self.soup_content: BeautifulSoup = BeautifulSoup(content, "html.parser")
         self.soup_content = self.soup_content.find("div", attrs={'class': 'listview'})
-        self.item_list = self.soup_content.find_all("a", attrs={'class': re.compile("q")}, href=re.compile("item="))
+        self.item_list_data: ResultSet = \
+            self.soup_content.find_all("a", attrs={'class': re.compile("q")}, href=re.compile("item="))
+
         # Closing Virtual Chrome browser after we are done
         browser.quit()
 
     def get_item_list(self):
-        nice_item_list = {}
-        for x in self.item_list:
-            nice_item_list[x.text] = x['href']
-        return nice_item_list
+        item_link_list: dict[str, str] = {}
+        for x in self.item_list_data:
+            if x.text == "":
+                print("Failed to find item name in the list:", self.item_list_data)
+                continue
+            item_link_list[x.text] = x['href']
+        return item_link_list
 
 
-def _get_chrom_driver(service):
+class LinkType(enum.Enum):
+    SINGLE_ITEM = 0
+    ITEM_LIST = 1
+
+
+def _get_link_type(html: str) -> LinkType:
+    # wowhead_link = "https://www.wowhead.com/wotlk"
+    single_item_link = "https://www.wowhead.com/wotlk/item"
+    item_list_link = "https://www.wowhead.com/wotlk"
+    if html == single_item_link:
+        print("Link type is single")
+        return LinkType.SINGLE_ITEM
+
+    if html == item_list_link:
+        print("Link type is item list")
+        return LinkType.ITEM_LIST
+
+    print("Link type is invalid")
+    raise Exception("Invalid Link")
+
+    # throw error
+
+
+def _get_chrom_driver(service: Service) -> WebDriver:
     chrome_options = webdriver.ChromeOptions()
 
     # Making the browser run in background and invisible
@@ -194,4 +252,3 @@ def _get_chrom_driver(service):
     chrome_options.add_argument("disable-gpu")
 
     return webdriver.Chrome(service=service, options=chrome_options)
-
