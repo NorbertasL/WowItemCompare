27c8f420a01ccb988cff8eb61fd0838cf0f47534
NorbertasL <nlapenas@gmail.com>
2022-08-09 16:58:54 +0100

    Updated everything with typing

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-import json
-import re
 from pprint import pprint
-from tokenize import Comment
-
-from selenium import webdriver
+import output_beautifier
 from selenium.webdriver.chrome.service import Service
-from bs4 import BeautifulSoup
-import pandas as pd
-
-# Setting up Virtual Chrome
 from item_filter import Filter
+from item_obj import Item, ArmourClassOf, ItemStatsOf
 from item_parser import SingleItemParser
 from item_parser import ItemListParser
 
-googleDriverPath = "D:/Programming/ChromeDriver/chromedriver.exe"
-service = Service(googleDriverPath)
+googleDriverPath: str = "D:/Programming/ChromeDriver/chromedriver.exe"
+service: Service = Service(googleDriverPath)
 
-url = input("Enter WowHead URL:").strip()
+url: str = input("Enter WowHead URL:").strip()
 # url = "https://www.wowhead.com/wotlk/item=37651/the-prospectors-prize"
 while url.lower() != "q":
-    parser = ItemListParser(service, url)
-    returned_data = parser.get_item_list()
-
-
+    parser: ItemListParser = ItemListParser(service, url)
+    returned_data: dict = parser.get_item_list()
 
     pprint(returned_data)
     print("Total Found:", len(returned_data))
 
-    item_obj_list = []
+    item_obj_list: list[Item] = []
     for k, v in returned_data.items():
         print("Searching for ", k, " ...")
-        item_obj_list.append(SingleItemParser(service, v).get_item_data())
+        item: Item = SingleItemParser(service, v).get_item_data()
+        print(item)
+        item_obj_list.append(item)
 
-    my_filter = Filter()
-    my_filter.set_ignore_armour_class(("Plate", ""))
-    my_filter.set_ignore_stat(("Strength", "Agility", "Spirit"))
+    my_filter: Filter = Filter()
+    my_filter.set_ignore_armour_class({ArmourClassOf.PLATE})
+    my_filter.set_ignore_stat({ItemStatsOf.STR, ItemStatsOf.AGI, ItemStatsOf.ARP, ItemStatsOf.EXP})
 
     item_obj_list = my_filter.filter_items(item_obj_list)
 
     print("Total Found after Filter:", len(item_obj_list))
+
+    output_beautifier.to_exe_for_exel(item_obj_list, "|", "output.txt")
 
     print("Type q to quit")
     url = input("Enter WowHead URL:").strip()
