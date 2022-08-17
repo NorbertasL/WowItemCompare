d6003a78806136dcfec185d81c55d20c77ce0b4f
NorbertasL <nlapenas@gmail.com>
2022-08-17 11:41:37 +0100

    cleaned up main and added some cmds

--- main.py
+++ main.py
@@ -1,73 +1,17 @@
 from pprint import pprint
+
+import item_parser
 from personal_printer import TableMaker
 from selenium.webdriver.chrome.service import Service
 from item_filter import Filter
 from item_obj import Item, ArmourClassOf, ItemStatsOf, ItemBasicParameterOf, SocketColourOf, EquipmentSlotOf
-from item_parser import SingleItemParser
-from item_parser import ItemListParser
-from datetime import datetime
+from item_parser import SingleItemParser, ItemListParser, LinkType, get_link_type
 
-googleDriverPath: str = "D:/Programming/ChromeDriver/chromedriver.exe"
-service: Service = Service(googleDriverPath)
 
-url: str = input("Enter WowHead URL:").strip()
-
-# url = "https://www.wowhead.com/wotlk/item=37651/the-prospectors-prize"
-while url.lower() != "q":
-
-    # TODO TIMER
-    app_start_time: datetime = datetime.now()
-
-    # TODO TIMER
-    item_list_parser_start_time: datetime = datetime.now()
-
-    parser: ItemListParser = ItemListParser(service, url)
-    returned_data: dict = parser.get_item_list()
-
-    # TODO TIMER
-    item_list_parser_total_time = datetime.now() - item_list_parser_start_time
-
-    print("Item List Parse Time:", item_list_parser_total_time)
-
-    pprint(returned_data)
-    print("Total Found:", len(returned_data))
-
-    # TODO TIMER
-    item_parser_start_time: datetime = datetime.now()
-
-    item_obj_list: list[Item] = []
-    for k, v in returned_data.items():
-        print("Searching for ", k, " ...")
-        item: Item = SingleItemParser(service, v).get_item_data()
-        item_obj_list.append(item)
-
-    # TODO TIMER
-    item_parser_total_time = datetime.now() - item_parser_start_time
-
-    print("Item Parse Time:", item_parser_total_time)
-
-    # TODO TIMER
-    filter_start_time_start: datetime = datetime.now()
-
-    # Building filter
-    my_filter: Filter = Filter()
-    my_filter.set_ignore_armour_class({ArmourClassOf.PLATE})
-    my_filter.set_ignore_stat({ItemStatsOf.STR, ItemStatsOf.AGI, ItemStatsOf.ARP, ItemStatsOf.EXP, ItemStatsOf.ATP,
-                               ItemStatsOf.DEF_RARING, ItemStatsOf.SPT})
-    # END of filter
-
-    item_obj_list = my_filter.filter_items(item_obj_list)
-
-    # TODO TIMER
-    filter_total_time = datetime.now() - filter_start_time_start
-
-    print("Total Found after Filter:", len(item_obj_list))
-
-    # TODO TIMER
-    table_printer_start_time: datetime = datetime.now()
-
+def save(item_list: list[Item], file_name: str):
     # Building table layout
     table_printer: TableMaker = TableMaker()
+    table_printer.set_file_loc(file_name)
     table_printer.add_display_column(ItemBasicParameterOf.TYPE)
     table_printer.add_display_column(ItemBasicParameterOf.ARMOUR_CLASS)
     table_printer.add_display_column(ItemStatsOf.INT)
@@ -82,17 +26,99 @@ while url.lower() != "q":
     table_printer.add_display_column(SocketColourOf.BONUS)
     # End of table layout
 
-    table_printer.print_to_txt(item_obj_list)
+    table_printer.print_to_txt(item_list)
 
-    # TODO TIMER
-    table_printer_total_time = datetime.now() - table_printer_start_time
 
-    # TODO Printing out times
-    print("item_list_parser_total_time:", item_list_parser_total_time)
-    print("item_parser_total_time:", item_parser_total_time)
-    print("filter_total_time:", filter_total_time)
-    print("table_printer_total_time:", table_printer_total_time)
-    print("app_run_total_time:", datetime.now() - app_start_time)
+googleDriverPath: str = "D:/Programming/ChromeDriver/chromedriver.exe"
+service: Service = Service(googleDriverPath)
 
-    print("Type q to quit")
-    url = input("Enter WowHead URL:").strip()
+all_item_obj: list[Item] = []
+
+while True:
+    user_input: str = input("Enter WowHead URL/cmd:").strip()
+
+    link_type: LinkType = get_link_type(user_input)
+    if link_type is LinkType.INVALID:
+        # Handle commands in the match statement
+        user_input_tuple: tuple = tuple(user_input.lower().split(" "))
+        match user_input_tuple[0]:
+            case "q":
+                print("Are you sure you want to quit?Unsaved items will be lost:")
+                user_input = input()
+                if user_input.lower().strip().startswith("y"):
+                    print("Thank you for using my app...Goodbye!")
+                    break
+            case "len":
+                print("Item list len:", len(all_item_obj))
+
+            case "print":
+                print("-" * 30)
+                print("Item list")
+                for i in all_item_obj:
+                    print(i.get_basic_parameter(ItemBasicParameterOf.NAME),
+                          " ",
+                          i.get_basic_parameter(ItemBasicParameterOf.LINK))
+
+                print("-"*30)
+
+            case "save":
+                file = "MyTable.txt"
+                if len(user_input_tuple) >= 2:
+                    file = user_input_tuple[1]
+                save(all_item_obj, file)
+                print("Saving to:", file)
+
+            case _:
+                print(user_input, " is an unknown command or an invalid wowhead link!")
+
+        continue
+
+    print("Searching...")
+
+    if link_type is LinkType.SINGLE_ITEM:
+        # Handling single item parse
+        single_item_parser: SingleItemParser = SingleItemParser(service, user_input)
+        single_item: Item = single_item_parser.get_item_data()
+        print("Found:", single_item.get_basic_parameter(ItemBasicParameterOf.NAME))
+        print("Adding item to item list...")
+        all_item_obj.append(single_item)
+
+    elif link_type is LinkType.ITEM_LIST:
+        # Handle item list
+        item_list_parser: ItemListParser = ItemListParser(service, user_input)
+        item_link_list: dict = item_list_parser.get_item_list()
+        print("Total Items Found:", len(item_link_list))
+        print("Starting to parse individual items...")
+        item_obj_list: list[Item] = []
+        for k, v in item_link_list.items():
+            print("Searching for ", k, " ...")
+            item: Item = SingleItemParser(service, v).get_item_data()
+            item_obj_list.append(item)
+        print("Item search has finished!")
+        print("Adding items to item list...")
+        all_item_obj = [*all_item_obj, *item_obj_list]
+
+    else:
+        print("WTF just happened? Cannot figure out the link type, HELP!")
+        print(user_input, " was assumed to be:", link_type.name)
+        continue
+
+    print("Done! Total items in the list:", len(all_item_obj))
+
+
+"""""
+
+    # Building filter
+    my_filter: Filter = Filter()
+    my_filter.set_ignore_armour_class({ArmourClassOf.PLATE})
+    my_filter.set_ignore_stat({ItemStatsOf.STR, ItemStatsOf.AGI, ItemStatsOf.ARP, ItemStatsOf.EXP, ItemStatsOf.ATP,
+                               ItemStatsOf.DEF_RARING, ItemStatsOf.SPT})
+    # END of filter
+
+    item_obj_list = my_filter.filter_items(item_obj_list)
+
+    print("Total Found after Filter:", len(item_obj_list))
+
+    
+    
+"""
