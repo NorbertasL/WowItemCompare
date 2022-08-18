29fef69ff09bd1b18f7ac9042274ff211623889b
NorbertasL <nlapenas@gmail.com>
2022-08-18 22:23:28 +0100

    More content enums with keys and ids

--- ITEM_CONSTANTS.py
+++ ITEM_CONSTANTS.py
@@ -19,6 +19,24 @@ class JsonEnum(Enum):
             return self.value[0]
         return self.value
 
+class IdEnum(Enum):
+    def get_id(self) -> None | int:
+        if isinstance(self.value, tuple):
+            return self.value[1]
+        return None
+
+    @classmethod
+    def find_by_id(cls, slot_id: int) -> None | Enum:
+        for e in cls:
+            if slot_id == e.get_id():
+                return e
+        return None
+
+    def get_name(self) -> str:
+        if isinstance(self.value, tuple):
+            return self.value[0]
+        return self.value
+
 
 IGNORED_JSON_KEYS: list[str] = ["classs", "classes", "reqclass", "flags2", "quality", "reqlevel", "appearances",
                                 "displayid", "dps", "speed", "dmgmax1", "dmgmin1", "dmgrange", "dmgtype1", "dura",
@@ -29,7 +47,7 @@ IGNORED_JSON_KEYS: list[str] = ["classs", "classes", "reqclass", "flags2", "qual
 class ItemBasicParameterOf(JsonEnum):
     # ENUM = Name, Json_Key
 
-    TYPE = "Type", "subclass"
+    ARMOUR_CLASS = "Armour Class", "subclass"
     NAME = "Name", "name"
     LINK = "Link"
 
@@ -37,16 +55,17 @@ class ItemBasicParameterOf(JsonEnum):
     ITEM_LEVEL = "Item Level", "level"
     SLOT = "Slot", "slot"
 
-
     ARMOUR_VALUE = "Armour Value", "armor" # not used here see ItemStatsOf
     BINDING_TYPE = "Binding Type"
-    ARMOUR_CLASS = "Armour Class"
     USE_EFFECT = "Use Effects"
     PROC_EFFECT = "Proc Effects"
     SOURCE = "Source", "source"
     SOURCE_MORE = "Source More", "sourcemore"
 
-
+class ItemSlotOf(IdEnum):
+    HANDS = "Hands", 10
+    TRINKET = "Trinket", 12
+    TWO_HANDED = "Two-Hand", 17
 
 
 class ItemStatsOf(JsonEnum):
@@ -62,7 +81,6 @@ class ItemStatsOf(JsonEnum):
     HASTE = "Haste", "hastertng"
     CRIT = "Critical", "critstrkrtng"
     MP5 = "Mana Per 5", "manargn"
-    SP = "Spell Power"
     SD = "Spell Damage", "spldmg"
     HP = "Healing Power", "splheal"
     EXP = "Expertise", "exprtng"
@@ -74,64 +92,23 @@ class ItemStatsOf(JsonEnum):
     PARRY_RATING = "Parry Rating", "parryrtng"
     SHIELD_BLOCK_RATING = "Shield Block Rating", "blockrtng"
     BLOCK_VALUE = "Block Value", "blockamount"
-    RES = "Resilience"
+    RES = "Resilience", "resirtng"
 
     SOCKET_COUNT = "Socket Count", "nsockets"
     RED_SOCKET = "Red", "socket1"
-    YELLOW_SOCKET = "Yellow", "socket2"
-    BLUE_SOCKET = "Blue", "socket3"
+    BLUE_SOCKET = "Blue", "socket2"
+    YELLOW_SOCKET = "Yellow", "socket3"
+
     SOCKET_BONUS = "Socket Bonus", "socketbonus"
 
     ARMOUR_VALUE = "Armour Value", "armor"
     CD = "Cooldown", "cooldown"
+    ITEM_SET = "Item Set", "itemset"
 
 
+class ArmourClassOf(IdEnum):
+    CLOTH = "Cloth", 1
+    LEATHER = "Leather", 2
+    MAIL = "Mail", 3
+    PLATE = "Plate", 4
 
-class ArmourClassOf(Enum):
-    CLOTH = "Cloth"
-    LEATHER = "Leather"
-    MAIL = "Mail"
-    PLATE = "Plate"
-    NONE = "None"
-    UNKNOWN = "Unknown"
-
-
-# Will use this for sorting by equipment slot, when making tables
-class EquipmentSlotOf(Enum):
-    # Armour
-    HEAD = "Head"
-    NECK = "Neck"
-    SHOULDERS = "Shoulders"
-    CHEST = "Chest"
-    WRIST = "Wrist"
-    LEGS = "Legs"
-    FEET = "Feet"
-    WAIST = "Waist"
-    HANDS = "Hands"
-    BACK = "Back"
-
-    # Jewellery
-    FINGER = "Finger"
-    TRINKET = "Trinket", -3
-
-    # Special
-    RELIC = "Relic"
-
-    # Enum methods
-
-    def get_id(self):
-        if isinstance(self.value, tuple):
-            return self.value[1]
-        return None
-
-    @classmethod
-    def find_by_id(cls, slot_id: int):
-        for e in cls:
-            if slot_id == e.get_id():
-                return e
-        return None
-
-    def get_name(self):
-        if isinstance(self.value, tuple):
-            return self.value[0]
-        return self.value
